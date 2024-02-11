305ea038949e690e8b5f5cf3a9a052886b91c7d1
Saksham-13 <sakshamalok13@gmail.com>
2024-02-11 22:12:00 +0530

    somethin

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,41 @@ interface User {
   phone : string;
 }
 async function getUsers() : Promise<User[]> {
-  const res = await fetch("http://localhost:3000/api/User",{ cache: 'no-store' });
-
-  const users = await res.json();
-  
+  // const res = await fetch("http://localhost:3000/api/User",{ cache: 'no-store' });
+ 
+  const users = [
+    {
+      id: 1,
+      email: 'guglu000@gmail.com',
+      name: 'Saksham Alok',
+      password: 'admin',
+      phone: '7905260207',
+      createdAt: '2023-11-22T05:07:21.461Z',
+      updatedAt: '2023-11-22T05:06:40.776Z',
+      deletedAt: null
+    },
+    {
+      id: 2,
+      email: 'samanthasusanjohn20@gmail.com',
+      name: 'Samantha John',
+      password: 'Samantha',
+      phone: '7905260208',
+      createdAt: '2023-11-22T05:41:37.865Z',
+      updatedAt: '2023-11-22T05:41:37.865Z',
+      deletedAt: null
+    },
+    {
+      id: 3,
+      email: 'nishthap981@gmail.com',
+      name: 'Nishtha P',
+      password: 'Nishtha',
+      phone: '9844902272',
+      createdAt: '2023-11-22T05:43:31.400Z',
+      updatedAt: '2023-11-22T05:43:31.400Z',
+      deletedAt: null
+    }
+  ]
+  console.log(users);
   return users;
   
 }
